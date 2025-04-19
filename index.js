c2742ee958c1601356d90597455c28abdd620063
Thomas Manong'a <thomasmanonga7@gmail.com>
2025-04-19 16:50:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const API_KEY = "http://www.omdbapi.com/?i=tt3896198&apikey=7bb39f84"
+const searchBtn = document.getElementById("searchBtn")
+
+async function searchMovies() {
+    const query = document.getElementById("search").value;
+    const response = await fetch(`${API_KEY}&s=${encodeURIComponent(query)}`);
+    const data = await response.json();
+
+    const movielist = document.getElementById("list")
+    console.log(data)
+    if(data.Response === "True"){
+        movielist.innerHTML = ""
+        data.Search.forEach(movie => {
+            const movieItem = document.createElement("div")
+            movieItem.classList.add("card")
+            movieItem.innerHTML = `
+                <img src="${movie.Poster}" alt="${movie.Title}">
+                <div class="movie-info">
+                    <h3>${movie.Title}</h3>
+                    <p>Year: ${movie.Year}</p>
+                </div>
+            `
+            movielist.appendChild(movieItem)
+        })
+    }else {
+        const errorItem = document.createElement("div")
+        errorItem.classList.add("error-item")
+        errorItem.innerHTML = `<p>${data.Error}</p>`
+        movielist.appendChild(errorItem)
+    }
+}
+
+searchBtn.addEventListener("click", searchMovies)
